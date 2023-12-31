d075f69051eb942a5fb694ccc82ef617ec6b570c
Thomas Gransden <thomas.gransden@petsathome.co.uk>
2023-12-31 10:33:13 +0000

    Aggregate data function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,50 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-import {onRequest} from "firebase-functions/v2/https";
+import {https} from "firebase-functions/v1";
+import * as admin from "firebase-admin";
 import * as logger from "firebase-functions/logger";
+import {format} from "date-fns";
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+admin.initializeApp();
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const aggregateResults = https.onRequest(async (req, res) => {
+  if (req.headers.authorization) {
+    const splitToken = req.headers.authorization.split(" ")[1];
+    const decodedToken = await admin.auth().verifyIdToken(splitToken);
+
+
+    const userDb = admin.firestore().collection("users")
+      .doc(decodedToken.uid);
+
+    const result = await userDb.get();
+
+    const formattedData: {[k in string]: []} = [
+      ...(result.get("medications") ?? []),
+      ...(result.get("foods") ?? []),
+      ...(result.get("moods") ?? []),
+    ].reduce((acc, cur: {createdDate: string}) => {
+      const formattedDate = format(cur.createdDate, "yyyy-MM-dd");
+
+      if (acc[formattedDate]) {
+        acc[formattedDate] = [...acc[formattedDate], cur];
+      } else {
+        acc[formattedDate] = [cur];
+      }
+
+      return acc;
+    }, {});
+
+    const dataToReturn = Object.entries(formattedData)
+      .map(([date, entries]) => ({
+        displayDate: format(date, "EEEE do MMMM y"),
+        sortedEntries: entries
+          .sort((a: {createdDate: string}, b: {createdDate: string}) =>
+            b.createdDate.localeCompare(a.createdDate)),
+      }));
+
+    logger.info("Reduced thing: " + JSON.stringify(dataToReturn));
+
+
+    res.status(200).send({data: JSON.stringify(dataToReturn)});
+  } else {
+    res.status(403).send("No bearer token attached to request");
+  }
+});
