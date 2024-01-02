24362edb2c68b25e3cfaa20c9f7aee212f892b82
Thomas Gransden <thomasgransden@gmail.com>
2024-01-02 19:17:07 +0000

    Add pains to API response

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -17,17 +17,16 @@ export const aggregateResults = https.onRequest(async (req, res) => {
     const splitToken = req.headers.authorization.split(" ")[1];
     const decodedToken = await admin.auth().verifyIdToken(splitToken);
 
-
-    const userDb = admin.firestore().collection("users")
-      .doc(decodedToken.uid);
+    const userDb = admin.firestore().collection("users").doc(decodedToken.uid);
 
     const result = await userDb.get();
 
-    const formattedData: {[k in string]: []} = [
+    const formattedData: { [k in string]: [] } = [
       ...(result.get("medications") ?? []),
       ...(result.get("foods") ?? []),
       ...(result.get("moods") ?? []),
-    ].reduce((acc, cur: {createdDate: string}) => {
+      ...(result.get("pains") ?? []),
+    ].reduce((acc, cur: { createdDate: string }) => {
       const formattedDate = format(cur.createdDate, "yyyy-MM-dd");
 
       if (acc[formattedDate]) {
@@ -43,11 +42,12 @@ export const aggregateResults = https.onRequest(async (req, res) => {
       .map(([date, entries]) => ({
         displayDate: format(date, "EEEE do MMMM y"),
         date,
-        sortedEntries: entries
-          .sort((a: {createdDate: string}, b: {createdDate: string}) =>
-            b.createdDate.localeCompare(a.createdDate)),
-      })).sort((a, b) => new Date(b.date).getTime() -
-      new Date(a.date).getTime());
+        sortedEntries: entries.sort(
+          (a: { createdDate: string }, b: { createdDate: string }) =>
+            b.createdDate.localeCompare(a.createdDate)
+        ),
+      }))
+      .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
 
     res.status(200).send({data: JSON.stringify(dataToReturn)});
   } else {
