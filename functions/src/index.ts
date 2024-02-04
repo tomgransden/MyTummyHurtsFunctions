4bae32e52ffe86faa022aa7d26d3525c04660ebe
Thomas Gransden <thomas.gransden@petsathome.co.uk>
2024-02-04 21:54:28 +0000

    New func

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,6 +6,92 @@ import {v4 as uuidv4} from "uuid";
 
 admin.initializeApp();
 
+export const getSummaryData = https.onRequest(async (req, res) => {
+  if (req.headers.authorization) {
+    // Read the token from the request
+    const splitToken = req.headers.authorization.split(" ")[1];
+    const decodedToken = await admin.auth().verifyIdToken(splitToken);
+
+    // Get the document for the user
+    const userDb = admin.firestore().collection("users").doc(decodedToken.uid);
+
+    const userRecord = await userDb.get();
+
+    // Generate the last 7 days of data
+    const lastSevenDays = eachDayOfInterval({
+      start: new Date(),
+      end: subDays(Date.now(), 6),
+    }).map((date) => format(date, "yyyy-MM-dd"));
+
+    const allPains = [...(userRecord.get("pains") ?? [])];
+    const allBowels = [...(userRecord.get("bowel") ?? [])];
+    const allMoods = [...(userRecord.get("moods") ?? [])];
+    const allFoods = [...(userRecord.get("foods") ?? [])];
+
+    const getPainsForDay = (date: string) => {
+      const matchingPains = allPains.filter(({createdDate}) =>
+        isSameDay(createdDate, date)).map(({metadata}) => metadata.painScore);
+
+      if (matchingPains.length === 0) return null;
+
+      return matchingPains.reduce( ( p, c ) =>
+        p + c, 0 ) / matchingPains.length;
+    };
+
+    const getBowelsForDay = (date: string) => {
+      const matchingBowels = allBowels.filter(({createdDate}) =>
+        isSameDay(createdDate, date));
+
+      if (matchingBowels.length === 0) return null;
+
+      return matchingBowels.length;
+    };
+
+    const chartData = lastSevenDays.map((date) => ({
+      date: format(date, "E"),
+      pain: getPainsForDay(format(date, "yyyy-MM-dd")),
+      bowel: getBowelsForDay(format(date, "yyyy-MM-dd")),
+    })).reverse();
+
+    const formattedData: { [k in string]: [] } = [
+      ...(allFoods),
+      ...(allMoods),
+      ...(allPains),
+      ...(allBowels),
+    ].reduce((acc, cur: { createdDate: string }) => {
+      const formattedDate = format(cur.createdDate, "yyyy-MM-dd");
+
+      if (!lastSevenDays.includes(formattedDate)) return acc;
+
+      if (acc[formattedDate]) {
+        acc[formattedDate] = [...acc[formattedDate], cur];
+      } else {
+        acc[formattedDate] = [cur];
+      }
+
+      return acc;
+    }, {});
+
+    const summaryData = Object.entries(formattedData)
+      .map(([date, entries]) => ({
+        displayDate: format(date, "EEEE do MMMM y"),
+        date,
+        sortedEntries: entries.sort(
+          (a: { createdDate: string }, b: { createdDate: string }) =>
+            b.createdDate.localeCompare(a.createdDate)
+        ),
+      }))
+      .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+
+    res.status(200).send({data: {
+      chartData,
+      summaryData,
+    }});
+  } else {
+    res.status(403).send("No access token attached to request");
+  }
+});
+
 export const retrofitUniqueIds = https.onRequest(async (req, res) => {
   const {users} = await getAuth().listUsers();
 
